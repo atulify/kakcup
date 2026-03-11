0ec2f8c6768f131bd0c171c521d2b8f7de753410
Atul Sharma <atul.sharma@shopify.com>
2026-03-11 09:56:48 -0400

    disable gzip size reporting in vite build

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,5 +1,5 @@
 // Use fixed version for cache - update this when deploying
-const CACHE_VERSION = '73f1b3c';
+const CACHE_VERSION = 'eda108f';
 const CACHE_NAME = `kak-cup-${CACHE_VERSION}`;
 const STATIC_CACHE = 'kak-cup-static';
 
